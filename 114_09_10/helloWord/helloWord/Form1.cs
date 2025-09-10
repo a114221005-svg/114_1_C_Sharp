bd9f103e95235a640eb9677269003e687384227d
a114221005-svg <a114221005@mail.shu.edu.tw>
2025-09-10 12:57:01 +0800

    114_09_10

---
+++ 114_09_10/helloWord/helloWord/Form1.cs
@@ -0,0 +1,15 @@
+namespace helloWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "順利完成第一個專案";
+        }
+    }
+}
