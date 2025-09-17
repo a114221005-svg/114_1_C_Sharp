2fdf0e42035129d29a9dc89a588449bcac74241f
a114221005-svg <a114221005@mail.shu.edu.tw>
2025-09-17 13:03:44 +0800

    114_09_17

---
+++ 114_09_17/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messengerbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messengerbutton
+            // 
+            this.messengerbutton.Font = new System.Drawing.Font("新細明體", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.messengerbutton.Location = new System.Drawing.Point(268, 147);
+            this.messengerbutton.Name = "messengerbutton";
+            this.messengerbutton.Size = new System.Drawing.Size(258, 113);
+            this.messengerbutton.TabIndex = 0;
+            this.messengerbutton.Text = "顯示訊息";
+            this.messengerbutton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 451);
+            this.Controls.Add(this.messengerbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button messengerbutton;
+    }
+}
+
